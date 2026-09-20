800e1b6ed38a7df8b06b08f77d86a9465f3eb58b
youcefpy <youcefboutiche@hotmail.fr>
2026-09-20 16:36:36 +0200

    implement test and endpoint for get account

--- service/routes.py
+++ service/routes.py
@@ -68,7 +68,14 @@ def create_accounts():
 # READ AN ACCOUNT
 ######################################################################
 
-# ... place you code here to READ an account ...
+@app.route("/accounts/<id>", methods=["GET"])
+def read_account(id):
+    account = Account.find(id)
+    if account is None : 
+        return "Ressource Not Found", status.HTTP_404_NOT_FOUND
+    account = account.serialize()
+    return account , status.HTTP_200_OK
+
 
 
 ######################################################################
